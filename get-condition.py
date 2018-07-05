61b8dd388b2893bbb6ebaba8696ad1f44c9e3ffc
徐炜增 <xuweizeng@shein.com>
2018-07-05 19:47:07 +0800

    筛选条件爬虫文件调整，抽离方法；

--- get-condition.py
+++ get-condition.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from urllib import request
-import base64
-from zlib import decompress
 from bs4 import BeautifulSoup
 import re
+from function import request_html
 
 url_diamond = "http://www.zbird.com/diamond/"
-
-
-def request_html(page_url):
-    with request.urlopen(page_url) as f:
-        data = f.read()
-    res = data.decode('utf-8')
-    return res
-
-
-# 基于页面 ajax 接口返回的数据解密接口
-def request_api(api_url):
-    with request.urlopen(api_url) as f:
-        data = f.read()
-    res = base64.b64decode(data)
-    res = decompress(res, -8)
-    res = res.decode('utf-8')
-    return res
-
-
-def get_request_info(request_url):
-    with request.urlopen(request_url) as f:
-        return f.info()
-
 
 html = request_html(url_diamond)
 soup = BeautifulSoup(html, 'html.parser')
