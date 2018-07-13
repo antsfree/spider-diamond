e98bef81944aa045e206cb5aba12e2bcb56876b5
徐炜增 <xuweizeng@shein.com>
2018-07-13 15:34:29 +0800

    爬取的条件写文件；增加默认条件的 json 文件；

--- get-condition.py
+++ get-condition.py
@@ -3,11 +3,18 @@
 from bs4 import BeautifulSoup
 import re
 from function import request_html
+import os
+from json import dumps
 
 url_diamond = "http://www.zbird.com/diamond/"
+condition_file = "./data/condition.json"
 
 
-def main():
+def condition():
+    """
+    获取钻石查询参数及对应值
+    :return:
+    """
     html = request_html(url_diamond)
     soup = BeautifulSoup(html, 'html.parser')
     # list
@@ -50,7 +57,38 @@ def main():
             except Exception as e:
                 continue
 
+    return attr_map
+
+
+def store_condition_data(data, file):
+    """
+    查询条件存储
+    """
+    file_info = os.path.split(file)
+    # 文件路径 & 文件名
+    file_dir, file_name = file_info
+    # 判断路径
+    if not os.path.isdir(file_dir):
+        os.mkdir(file_dir)
+    # 判断文件
+    if not os.path.exists(file):
+        os.system(r'touch %s' % file)
+    # 写文件
+    with open(file, 'w+', encoding='utf-8') as f:
+        data = dumps(data)
+        f.write(data)
+        f.seek(0)
+        res = f.read()
+        return res
+
+
+def main():
+    attr_map = condition()
     print(attr_map)
+    # 写文件操作
+    store_condition_data(attr_map, condition_file)
+    if os.path.exists(condition_file):
+        print('Successful access to conditions !')
 
 
 if __name__ == '__main__':
